ff57faab1afb253261f353f89c8af3eef455ada7
Paras Chawla <paraschawla.2002@gmail.com>
2024-05-31 12:59:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Nav from "./components/Nav"
+import "./App.css"
+
+export default function App(){
+  return(
+    <div>
+      <Nav />
+    </div>
+  )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+
+export default function Nav(){
+    return(
+        <nav>
+            <img src="/images/Mini-Logo.png" alt="Mini-Logo" className="mini-logo" />
+            <ul className="nav-list">
+                <li href="#">MODELS</li>
+                <li href="#">FINANCE</li>
+                <li href="#">SERVICE</li>
+                <li href="#">FIND A MINI CENTRE</li>
+                <li href="#">TEST DRIVE</li>
+                <li href="#">BUY</li>
+            </ul>
+        </nav>
+    )
+}
